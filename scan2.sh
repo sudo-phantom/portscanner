a5e498d2c5d8ecb7970ae73ab5515a08f4cd2055
sudo-phantom <sudophantom@gmail.com>
2019-09-12 09:51:37 -0700

    Add files via upload

---
+++ scan2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+host=$1
+firstport=$2
+lastport=$3
+
+function portscan
+
+{
+for ((counter=$firstport; counter<=$lastport; counter++))
+do
+ (echo >/dev/tcp/$host/$counter) > /dev/null 2>&1 && echo "$counter open"
+done
+}
+
+portscan
